9e781a2473c8bda8ad3d5d90219f9c3e72bf7f0b
Srivignesh K <srivigneshkathirvel@gmail.com>
2026-07-09 18:34:01 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (80.68%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,10 +1,5 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        if(num==6 || num==28 || num==496 || num==8128 || num==33550336 ){
-           return true;
-        }
-        else{
-            return false;
-        }
+        return (num==6 || num==28 || num==496 || num==8128 || num==33550336) ? true : false;
     }
 }
